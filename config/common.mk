765f076af01b071bfc1a2b91359e3e77725ba234
Danny Lin <danny@kdrag0n.dev>
2021-10-09 06:24:31 -0700

    vendor: Enable one-handed mode
    There's nothing device-specific about this feature, so enable it for
    everyone.

--- config/common.mk
+++ config/common.mk
@@ -50,6 +50,10 @@ $(foreach f,$(wildcard vendor/ppui/prebuilt/common/etc/init/*.rc),\
 PRODUCT_COPY_FILES += \
     vendor/ppui/config/permissions/android.software.nfc.beam.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.software.nfc.beam.xml
 
+# Enable one-handed mode
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.support_one_handed_mode=true
+
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
