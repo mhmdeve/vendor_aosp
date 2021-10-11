09f7657c06574f665d7d384bb727f37b77fe1ac8
jhenrique09 <jhsv09@gmail.com>
2021-10-11 13:54:11 -0300

    vendor: Restore some stuffs
    Change-Id: Ia00ac803eb9d7114512c7ab2ee4155a82211a071

--- config/common.mk
+++ config/common.mk
@@ -198,7 +198,7 @@ $(call inherit-product, vendor/ppui/config/audio.mk)
 $(call inherit-product, vendor/ppui/config/bootanimation.mk)
 
 # Fonts
-#$(call inherit-product, vendor/ppui/config/fonts.mk)
+$(call inherit-product, vendor/ppui/config/fonts.mk)
 
 # GApps
 #$(call inherit-product, vendor/gapps/config.mk)
@@ -207,6 +207,6 @@ $(call inherit-product, vendor/ppui/config/bootanimation.mk)
 $(call inherit-product, vendor/ppui/config/ota.mk)
 
 # RRO Overlays
-#$(call inherit-product, vendor/ppui/config/rro_overlays.mk)
+$(call inherit-product, vendor/ppui/config/rro_overlays.mk)
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
